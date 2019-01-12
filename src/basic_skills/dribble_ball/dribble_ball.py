5d282180df1ac38b14c3244eaadd0bed034dfe83
Nathan Pestes <nathan.pestes@gmail.com>
2019-01-12 11:49:37 -0800

    Comments and made the import local

--- src/basic_skills/dribble_ball/dribble_ball.py
+++ src/basic_skills/dribble_ball/dribble_ball.py
@@ -2,11 +2,10 @@ import numpy as np
 import math
 import sys
 #replace this with your path to robocup-ai
-sys.path.insert(0, '/Users/nathan/Documents/robocup-ai/src')
+sys.path.insert(0, '../..')
 from basic_skills.action import *
 from basic_skills.move_to.move_to import *
 from basic_skills.helper_functions import *
-from matplotlib.widgets import Slider, Button, RadioButtons
 from pygame_simulator.PySim_noise import *
 
 robot_rotation_speed = 4.25
@@ -14,7 +13,8 @@ robot_rotation_speed = 4.25
 this is basically a reskin of orbit ball with parameters tuned so that it pushes the ball instead of orbiting it
 '''
 class dribble_ball(action):
-  #covers a pass from target_robot to target_loc
+  #reskin of orbit_ball, offset is 90 (ball radius is 120)
+  #makes the robot push into the ball
   def __init__(self, target_loc = False, offset = 90):
     action.__init__(self)
     self.pid = move_to()
@@ -32,28 +32,34 @@ class dribble_ball(action):
     self.pid.add(robot, game)
     action.add(self, robot, game)
   def run(self):
-    ball_extrapolation = self.game.ball.loc# + self.game.ball.velocity/10
+    #pull from current location in by spiral factor
+    ball_extrapolation = self.game.ball.loc
     robot_vec = self.robot.loc - ball_extrapolation
-    #print(self.robot.loc, ball_extrapolation)
     robot_vec_scaled = robot_vec * self.offset / np.linalg.norm(robot_vec)
     target_loc = robot_vec_scaled * (1 - self.spiral_factor) + self.spiral_factor * robot_vec
     
+    #get desired global angle and use the difference between that and current angle to rotate "target_loc"
+    #by a scaled amount
     target_vec = ball_extrapolation - self.target_loc
     current_angle = -math.atan2(robot_vec[1], robot_vec[0])
-    target_angle = math.atan2(-target_vec[1], target_vec[0])
+    target_angle = -math.atan2(target_vec[1], target_vec[0])
     rotation_angle = -min_angle(current_angle - target_angle) * (1 - self.spiral_factor) / 2
-    #print(37, rotation_angle, current_angle, target_angle)
     orbit_vec = convert_local(target_loc, rotation_angle)
+    
+    #if the move_to location is too close to the robot push it out by an amount
+    #this amout increases by push_speed_factor if we are pushing the ball
+    #and by chase_down_factor if we are behind where we want to be on the ball
+    #speed_mod_factor is a baseline constant pushout factor
     move_to = orbit_vec + ball_extrapolation
     speed_mod_vec = move_to - self.robot.loc
-    #print(abs(rotation_angle))
     on_angle_factor = np.clip(math.pi - 10*abs(rotation_angle), 0, math.pi)**2
     off_angle_factor = np.clip(-math.pi/20 + abs(rotation_angle), 0, math.pi)**2
-    #print(on_angle_factor, off_angle_factor, np.linalg.norm(speed_mod_vec))
     if np.linalg.norm(speed_mod_vec) < 300:
       move_to = move_to + speed_mod_vec * (self.speed_mod_factor + self.push_speed_factor * on_angle_factor
                 + off_angle_factor * self.chase_down_factor)
     
+    #look at the ball 
+    #call pid
     point_dir = (ball_extrapolation) - self.robot.loc
     target_rot = -math.atan2(point_dir[1], point_dir[0])
     self.pid.set_target(move_to, target_rot)
