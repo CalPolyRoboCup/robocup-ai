3db573c85f3b461d30eb355f059c7d1f8119e350
hrinn <haydenrinn@gmail.com>
2019-02-02 13:35:49 -0800

    Fixes #19 Strategy Interface Improvements

--- src/strategy_execution/src/Strategy.py
+++ src/strategy_execution/src/Strategy.py
@@ -1,4 +1,27 @@
 class Strategy:
+	zoneDict = {
+				1: self.rule_zone_1,
+				2: self.rule_zone_2,
+				3: self.rule_zone_3,
+				4: self.rule_zone_4,
+				5: self.rule_zone_5,
+				6: self.rule_zone_6,
+				7: self.rule_zone_7,
+				8: self.rule_zone_9,
+				9: self.rule_zone_9,
+				10: self.rule_zone_10,
+				11: self.rule_zone_11,
+				12: self.rule_zone_12,
+				13: self.rule_zone_13,
+				14: self.rule_zone_14,
+				15: self.rule_zone_15,
+				16: self.rule_zone_16,
+				17: self.rule_zone_17,
+				18: self.rule_zone_18,
+				19: self.rule_zone_19,
+				20: self.rule_zone_20
+			}
+
 	def rule_zone_1(self, Player p):
 		pass
 
@@ -60,26 +83,4 @@ class Strategy:
 		pass
 
 	def apply(self, Player p):
-		zoneDict = { #hacky python switch statement
-			1: self.rule_zone_1,
-			2: self.rule_zone_2,
-			3: self.rule_zone_3,
-			4: self.rule_zone_4,
-			5: self.rule_zone_5,
-			6: self.rule_zone_6,
-			7: self.rule_zone_7,
-			8: self.rule_zone_9,
-			9: self.rule_zone_9,
-			10: self.rule_zone_10,
-			11: self.rule_zone_11,
-			12: self.rule_zone_12,
-			13: self.rule_zone_13,
-			14: self.rule_zone_14,
-			15: self.rule_zone_15,
-			16: self.rule_zone_16,
-			17: self.rule_zone_17,
-			18: self.rule_zone_18,
-			19: self.rule_zone_19,
-			20: self.rule_zone_20,
-		}
-		zoneDict[p.zone](p) #selecting a zone is now O(1)
+		Strategy.zoneDict[p.zone](p)
