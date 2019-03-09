aa871bf11cbe4ea643c496708d0a5ab55bed8f1d
hrinn <haydenrinn@gmail.com>
2019-03-09 13:32:01 -0800

    references #40 coach node publishes and subscribes to the correct messages

--- src/coach/scripts/coach.py
+++ src/coach/scripts/coach.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
 
 import rospy
+from robocup_msgs.msg import *
+from std_msgs.msg import String
 
 def coach():
-	pub = rospy.Publisher('strategy', Strategy, queue_size=10)
-	#Topic: strategy
-	#Message type: Strategy (exists in msg but not currently defined)
-	rospy.init_node('coach', anonymous = True)
-	rospy.Subscriber('locations', GameData, callback)
-	rate = rospy.Rate(10) #10 Hz
+	rospy.init_node("coach")
+
+	#Coach node subscribes to the locations topic
+	rospy.Subscriber("locations", Position, callback)
+
+	rospy.Subscriber("test", String, callback2)
+
+	# Coach node publishes to the strategy topic
+	pub = rospy.Publisher("strategy", Strategy, queue_size=10)
+	rate = rospy.Rate(10) # 10 Hz
+
+	#Define the Strategy message
+	msg = Strategy()
+	msg.strategy = 0
+	msg.additional_rules = 0
+
+	#Publish the Strategy message to the strategy topic
 	while not rospy.is_shutdown():
-		currentStrat = None #temp
-		rospy.loginfo(currentStrat)
-		pub.publish(currentStrat)
+		#rospy.loginfo(msg)
+		pub.publish(msg)
 		rate.sleep()
-	rospy.spin()
+	
+
+#Process the Position message from the locations topic
+def callback(Position):
+	rospy.loginfo(Position.poses, Position.twists)
+
+def callback2(data):
+	rospy.loginfo(data)
 
 if __name__ == '__main__':
 	try:
